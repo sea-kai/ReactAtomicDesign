61eed62e217208e08d0c8109e752f5bd9686ede0
sea-kai <76773825+sea-kai@users.noreply.github.com>
2021-07-27 02:41:53 +0900

    feat : グローバルなstate管理 バケツリレー useContext recoil

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,12 +1,13 @@
+import { memo } from "react";
 import styled from "styled-components";
 import { Card } from "../../atoms/card/Card";
 import { UserIconWithName } from "../../molecules/user/UserIconWithName";
 
-export const UserCard = (props) => {
+export const UserCard = memo((props) => {
   const { user } = props;
   return (
     <Card>
-      <UserIconWithName image={user.image} name={user.name} />
+      <UserIconWithName src={user.src} name={user.name} />
       <SDL>
         <dl>
           <dt>メール</dt>
@@ -21,7 +22,7 @@ export const UserCard = (props) => {
       </SDL>
     </Card>
   );
-};
+});
 
 const SDL = styled.dl`
   text-align: left;
